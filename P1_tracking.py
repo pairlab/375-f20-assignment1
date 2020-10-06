596902afb72a6bd5356bbcc89e65cb4e849654f9
Dylan Turpin <9turpin@gmail.com>
2020-10-06 16:42:29 -0400

    initial commit

---
+++ P1_tracking.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+########
+# compute dv (acceleration control) from reference joint trajectory
+# params:
+#   - dv_ref: reference acceleration
+#   - v: actual velocity
+#   - v_ref: reference velocity
+#   - q: actual joint configuration
+#   - q_ref: reference joint configuration
+#   - Kp: proportional gain matrix
+#   - Kd: derivative gain matrix
+def compute_jsid_dv(dv_ref, v, v_ref, q, q_ref, Kp, Kd):
+    ### ================ YOUR CODE STARTS ==========================
+    dv = None
+    ### ================  YOUR CODE ENDS  ==========================
+    return dv
+
+########
+# compute tau (torque) from desired joint acceleration
+# params:
+#   - dv_desired: desired acceleration
+#   - M: mass matrix
+#   - h: external forces
+def compute_jsid_tau(dv_desired, M, h):
+    ### ================ YOUR CODE STARTS ==========================
+    tau = None
+    ### ================  YOUR CODE ENDS  ==========================
+    return tau
+
+########
+# compute tau (torque) from desired task space acceleration
+# params:
+#   - ddx_desired: desired task space acceleration
+#   - J: jacobian of forward graphics
+#   - v: joint space velocity
+#   - M: mass matrix
+#   - h: external forces
+def compute_tsid_tau(ddx_desired, J, dJ, v, M, h):
+    ### ================ YOUR CODE STARTS ==========================
+    tau = None
+    ### ================  YOUR CODE ENDS  ==========================
+    return tau
---
+++ README.md
@@ -0,0 +1,19 @@
+# Install instructions
+
+Add conda-forge to the list of channels.
+
+```
+conda config --append channels conda-forge
+```
+
+Install all requirements to a new environment.
+
+```
+conda create --name 375a1 --file requirements.txt
+```
+
+Activate the new environment before running code.
+
+```
+conda activate 375a1
+```
